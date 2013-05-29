7e6d7b5b57711ad218009cc5bbbf36200b9065e8
Matthias Meulien <orontee@gmail.com>
2013-05-29 23:05:24 +0200

    Expenditure form cleanup

--- site/tracker/forms.py
+++ site/tracker/forms.py
@@ -3,21 +3,49 @@ from tracker.models import Expenditure
 from django.utils.translation import ugettext_lazy as _
 
 OCCURRENCES_CHOICES = (('1', _('unique')),
+                       ('2', _('next two months')),
+                       ('3', _('next three months')),
                        ('4', _('next four months')),
                        ('5', _('next five months')),
                        ('6', _('next six months')),
                        ('12', _('forthcoming year')))
 
+
 class ExpenditureForm(ModelForm):
-    """
-    Form to input an expenditure.
+    """Form to input an expenditure.
     """
     occurrences = ChoiceField(choices=OCCURRENCES_CHOICES,
-                             label=_('Occurrences'),
-                             help_text=
-                             _('The number of occurrences of the expenditure'))
+                              label=_('Occurrences'),
+                              help_text=
+                              _('The number of occurrences of the expenditure'))
+    other_dates = []
+
+    def clean(self):
+        """Form-wide cleaning. 
+
+        The usual cleaning is extended by the generation of dates from
+        the field named date by increasing its month as many times as
+        specified by the field named occurences.
+        """
+        cleaned_data = super(ExpenditureForm, self).clean()
+        count = int(cleaned_data['occurrences']) - 1
+        start = cleaned_data.get('date')
+        if start:
+            for delta in range(count):
+                month = start.month + 1 if start.month < 12 else 1
+                year = start.year + 1 if month == 1 else start.year
+                try:
+                    date = start.replace(month=month, year=year)
+                except ValueError:
+                    msg = _('All expenditures must occur on valid dates.')
+                    self._errors["occurrences"] = self.error_class([msg])
+                    del cleaned_data['occurrences']
+                    break
+                else:
+                    self.other_dates.append(date)
+        return cleaned_data
 
     class Meta(object):
+        """The data associated to the expenditure form.
+        """
         model = Expenditure
-        fields = ('amount', 'date', 'description', 'occurrences')
-
