3f0a604df8fe1c1c7c83cc813561e7dd188f51de
Matthias Meulien <orontee@gmail.com>
2013-06-28 02:08:59 +0200

    Propagation of number of rows per pages

--- site/tracker/templatetags/tracker_extras.py
+++ site/tracker/templatetags/tracker_extras.py
@@ -19,16 +19,43 @@ def check_date_month(date):
         return False
 
 
-@register.simple_tag(name='archive_url')
-def do_archive_url(date, page=1):
+@register.simple_tag(name='previous_month_url', takes_context=True)
+def do_previous_month_url(context):
+    """Return the archive url for the previous month.
+    """
+    return do_archive_url(context['previous_month'], 1,
+                          context.get('paginator', None))
+
+
+@register.simple_tag(name='current_month_url', takes_context=True)
+def do_current_month_url(context):
+    """Return the archive url for the current month.
+    """
+    return do_archive_url(datetime.today(), 1,
+                          context.get('paginator', None))
+
+
+@register.simple_tag(name='next_month_url', takes_context=True)
+def do_next_month_url(context):
+    """Return the archive url for the next month.
+    """
+    return do_archive_url(context['next_month'], 1,
+                          context.get('paginator', None))
+    
+
+def do_archive_url(date, page=1, paginator=None):
     """Return the archive url for the given date.
     """
-    template = '{0}?month={1}&year={2}&page={3}'
     url = reverse('tracker:archive')
+    template = '{0}?month={1}&year={2}&page={3}'
+    paginate_by = None
+    if paginator is not None:
+        paginate_by = paginator.per_page
+        template += '&paginate_by={4}'
     return template.format(url,
                            '{0:%m}'.format(date),
                            '{0:%Y}'.format(date),
-                           page)
+                           page, paginate_by)
 
 
 @register.simple_tag(name='pagination',
@@ -46,15 +73,16 @@ def do_pagination(context):
         # TODO Raise a user exception to signify a wrong use of the
         # tag
     else:
+        this_page = '<span class=\'current-page\'>{0}</span>'
         if is_paginated is False:
-            return '<span class=\'current-page\'>{0}</span>'.format(1)
+            return this_page.format(1)
         delta = 2
         elements = []
         template = u"""<a href='{url}' class='page-number' title='{msg}'>{number}</a>"""
         msg = _('Jump to page {0}')
 
         def add_element(i):
-            elements.append(template.format(url=do_archive_url(date, i),
+            elements.append(template.format(url=do_archive_url(date, i, paginator),
                                             msg=msg.format(i),
                                             number=i))
         hidden = min(page.number - delta - 1, delta) + 1
@@ -65,8 +93,7 @@ def do_pagination(context):
         visible = max(page.number - delta, 1)
         for i in range(visible, page.number):
             add_element(i)
-        elements.append(
-            '<span class=\'current-page\'>{0}</span>'.format(page.number))
+        elements.append(this_page.format(page.number))
         hidden = min(page.number + delta + 1, paginator.num_pages + 1)
         for i in range(page.number + 1, hidden):
             add_element(i)
