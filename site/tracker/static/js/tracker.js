7741116c3f181290d45ba245873158a3f31116b5
Matthias Meulien <orontee@gmail.com>
2013-09-27 00:07:17 +0200

    Tags in expenditure creation view

--- site/tracker/static/js/tracker.js
+++ site/tracker/static/js/tracker.js
@@ -1,6 +1,6 @@
 var Tracker = {};
 
-Tracker.toggle_menu = function(id) {
+Tracker.toggleMenu = function(id) {
     var node = document.getElementById(id);
     if (node) {
         if (node.style.visibility == 'hidden') {
@@ -15,5 +15,25 @@ Tracker.focus = function(id) {
     var node = document.getElementById(id);
     if (node) {
         node.focus();
+    }
+};
+
+Tracker.listenToTags = function(id) {
+    var target = document.getElementById(id);
+    if (target) {
+        var nodes = document.getElementsByClassName('tag');
+        for (var i = 0; i < nodes.length; i++) {
+            var node = nodes[i];
+            node.onclick = function(n) {
+                return function() {
+                    var value = target.value.trimRight();
+                    if (value.length) {
+                        target.value = value + ' ' + n.innerHTML.trim();
+                    } else {
+                        target.value = n.innerHTML.trim();
+                    }
+                };
+            }(node);
+        }
     }
 };
