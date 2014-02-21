825e7dc70235baae638b0ff013f890a6a12c0ed4
Matthias Meulien <orontee@gmail.com>
2014-02-21 01:20:26 +0100

    Fix circular import created by Django debug toolbar

--- site/purse/settings/local.py
+++ site/purse/settings/local.py
@@ -12,6 +12,13 @@ TEMPLATE_DEBUG = DEBUG
 
 INSTALLED_APPS = INSTALLED_APPS + ('debug_toolbar',)
 
+MIDDLEWARE_CLASSES = ('debug_toolbar.middleware.DebugToolbarMiddleware',) \
+                     + MIDDLEWARE_CLASSES
+
+INTERNAL_IPS = ('127.0.0.1',)
+
+DEBUG_TOOLBAR_PATCH_SETTINGS = False
+
 LOGGING.update({
     'handlers': {
         'console': {
--- site/purse/urls.py
+++ site/purse/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.conf.urls import (patterns, include, url)
 from django.views.generic.base import RedirectView
 from django.core.urlresolvers import reverse_lazy
@@ -8,3 +9,8 @@ urlpatterns = patterns('',
                                                  app_name='tracker')),
                        url(r'^$', RedirectView.as_view(
                            url=reverse_lazy('tracker:home'))))
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += patterns('',
+        url(r'^__debug__/', include(debug_toolbar.urls)),)
