0d198d909d48d8f418f1a6051b71d8697cac9269
Matthias Meulien <orontee@gmail.com>
2014-03-25 00:36:20 +0100

    Fix tag cloud

---
+++ site/tracker/static/js/tags.js
@@ -0,0 +1,42 @@
+var fill = d3.scale.category20();
+
+var margin = {top: 0, right: 20, bottom: 0, left: 20},
+    width = 600 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
+
+console.log(words);
+
+
+function draw(words) {
+    d3.select("#tags-body").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")")
+        .selectAll("text")
+        .data(words)
+        .enter().append("text")
+        .style("font-size", function(d) { return d.size + "px"; })
+        .style("font-family", "Impact")
+        .style("fill", function(d, i) { return fill(i); })
+        .attr("text-anchor", "middle")
+        .attr("transform", function(d) {
+            return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+        })
+        .text(function(d) { return d.text; })
+        .on("click", function(d) {
+            window.location = "/tracker/expenditures/search/?filter=" + d.text;
+        });
+}
+
+d3.layout.cloud().size([width + margin.left + margin.right,
+                       height + margin.top + margin.bottom])
+    .words(words)
+    .padding(5)
+    .rotate(function() { return ~~(Math.random() * 2) * 90; })
+    .font("Impact")
+    .fontSize(function(d) {
+        return 5 + d.size;
+    })
+    .on("end", draw)
+    .start();
