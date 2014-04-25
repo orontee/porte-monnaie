50c8e00e411d67516ba7279d55b70ca0f4f4dfec
Matthias Meulien <orontee@gmail.com>
2014-04-25 09:54:22 +0200

    Fix JavaScript style

--- site/tracker/static/js/tracker.js
+++ site/tracker/static/js/tracker.js
@@ -3,7 +3,7 @@ var Tracker = {};
 if(typeof String.prototype.trimRight !== 'function') {
   String.prototype.trimRight = function() {
     return this.replace(/\s+$/g, '');
-  }
+  };
 }
 
 Tracker.focus = function(id){
@@ -19,14 +19,15 @@ Tracker.listenToTags = function(id){
     var listener = function(event){
         var name = event.target.innerHTML.trim();
         var value = target.value.trimRight();
+        var node, i;
         if (value.length){
             target.value = value + ' ' + name;
         } else {
             target.value = name;
         }
     };
-    for (var i = 0; i < nodes.length; i++){
-        var node = nodes[i];
+    for (i = 0; i < nodes.length; i++){
+        node = nodes[i];
         node.addEventListener('click', listener);
     }
 };
